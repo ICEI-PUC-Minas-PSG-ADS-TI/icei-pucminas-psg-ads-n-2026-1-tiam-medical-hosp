46ad786cf0c9277e8f12aec4a3581eaa51516d54
lucasgabrielfer.oli@gmail.com <lucasgabrielfer.oli@gmail.com>
2026-05-02 18:29:11 -0300

    feat: add models

---
+++ src/MedicalHospAPI/Models/Calibracao.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalHospAPI.Models
+{
+    public class Calibracao
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public int periodicidade { get; set; }
+
+        [Required]
+        public bool status { get; set; }
+
+        [StringLength(255), Required]
+        public string certificadoPdf { get; set; }
+    }
+}
---
+++ src/MedicalHospAPI/Models/Padroes.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalHospAPI.Models
+{
+    public class Padroes
+    {
+        [Key]
+        public int id { get; set; }
+
+        [StringLength(255), Required]
+        public string nome { get; set; }
+
+        [StringLength(255), Required]
+        public string modelo { get; set; }
+
+        [StringLength(255), Required]
+        public string tag { get; set; }
+
+        [StringLength(255), Required]
+        public string numSerie{ get; set; }
+
+        [Required]
+        public bool ativo { get; set; }
+
+        [StringLength(255), Required]
+        public string patrimonio { get; set; }
+
+        [StringLength(255), Required]
+        public string setor{ get; set; }
+
+        [Required, ForeignKey("Calibracao")]
+        public int ultimaCalibracaoId { get; set; }
+        public Calibracao ultimaCalibracao { get; set; }
+    }
+}
