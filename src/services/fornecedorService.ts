03805ad500f745f4e69a3b888c8afcf1c15dd392
edimhelena <edimhelenaguimaraes@gmail.com>
2026-05-10 10:08:18 -0300

    feat: crud completo

--- src/services/fornecedorService.ts
+++ src/services/fornecedorService.ts
@@ -14,3 +14,60 @@ export async function getFornecedores(): Promise<Fornecedor[]> {
 
     return fornecedores;
 }
+
+export async function getFornecedorByID(id: string) {
+    const fornecedorRef = doc(db, collection_name, id);
+    const snapshot = await getDoc(fornecedorRef);
+
+    const fornecedor: Fornecedor = {
+        id: snapshot.id,
+        ...(snapshot.data() as Omit<Fornecedor, "id">)
+    }
+
+    return fornecedor;
+}
+
+export async function createFornecedor(novoFornecedor: FornecedorDTO): Promise<FornecedorDTO> {
+    await addDoc(fornecedoresRef, {
+        nome: novoFornecedor.nome,
+        razaoSocial: novoFornecedor.razaoSocial,
+        cnpj: novoFornecedor.cnpj,
+        responsavel: novoFornecedor.responsavel,
+        telefone: novoFornecedor.telefone,
+        email: novoFornecedor.email,
+        linkPortal: novoFornecedor.linkPortal,
+        usuarioPortal: novoFornecedor.usuarioPortal,
+        senhaPortal: novoFornecedor.senhaPortal
+    });
+
+    return novoFornecedor;
+}
+
+export async function updateFornecedor(id: string, fornecedorAtualizado: FornecedorDTO): Promise<FornecedorDTO> {
+    const fornecedorRef = doc(db, collection_name, id);
+    const fornecedorData = toFornecedorData(fornecedorAtualizado);
+
+    await updateDoc(fornecedorRef, fornecedorData);
+
+    return fornecedorAtualizado;
+}
+
+export async function deleteFornecedor(id: string) {
+    const fornecedorRef = doc(db, collection_name, id);
+
+    await deleteDoc(fornecedorRef);
+}
+
+function toFornecedorData(fornecedor: FornecedorDTO) {
+    return {
+        nome: fornecedor.nome,
+        razaoSocial: fornecedor.razaoSocial,
+        cnpj: fornecedor.cnpj,
+        responsavel: fornecedor.responsavel,
+        telefone: fornecedor.telefone,
+        email: fornecedor.email,
+        linkPortal: fornecedor.linkPortal,
+        usuarioPortal: fornecedor.usuarioPortal,
+        senhaPortal: fornecedor.senhaPortal
+    }
+}
